3b454db7096d468626842bb35eecb8431ea9bb53
vlova <nsinreal+vlova@gmail.com>
2020-05-04 13:32:25 +0300

    Added red test case

---
+++ tests/useCallbackTestCases/shouldUseCallbackWhenUseMakeCallback/input.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export const MyCounter = () => {
+    const [counter, setCounter] = React.useState(0);
+
+    return (
+        <div>
+            <span style={{ color: 'red' }}>{counter}</span>
+            <button onClick={makeIncrement(setCounter)}>+</button>
+        </div>
+    )
+};
+
+const makeIncrement = (setCounter: React.Dispatch<React.SetStateAction<number>>) => {
+    return () => setCounter(c => c + 1);
+}
