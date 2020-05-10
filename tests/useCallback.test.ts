e970321558c831fcf22f60bfe6f93422cbca21bd
vlova <nsinreal+vlova@gmail.com>
2020-05-10 18:29:08 +0300

    Added grouping of tests

--- tests/useCallback.test.ts
+++ tests/useCallback.test.ts
@@ -3,16 +3,24 @@ import { useCallbackTranformer } from '../useCallbackTransformer';
 import * as fs from 'fs';
 import { transformAndPrettyPrint } from './common';
 
+
+const testCasesBasePath = 'tests/useCallbackTestCases';
+const testCaseGroups = fs.readdirSync(testCasesBasePath);
+
 describe('useCallback', () => {
-    const testCasesBasePath = 'tests/useCallbackTestCases';
-    const testCases = fs.readdirSync(testCasesBasePath);
-    for (const testCaseName of testCases) {
-        const inputTSX = fs.readFileSync(`${testCasesBasePath}/${testCaseName}/input.tsx`).toString();
-        const outputTSX = fs.readFileSync(`${testCasesBasePath}/${testCaseName}/output.tsx`).toString();
-        it(testCaseName, () => {
-            const expectedOutputTSX = transformAndPrettyPrint(outputTSX, []);
-            const actualOutputTSX = transformAndPrettyPrint(inputTSX, [useCallbackTranformer]);
-            expect(actualOutputTSX).equal(expectedOutputTSX);
+    for (const testCaseGroup of testCaseGroups) {
+        describe(testCaseGroup, () => {
+            const testCasesBasePath = `tests/useCallbackTestCases/${testCaseGroup}`;
+            const testCases = fs.readdirSync(testCasesBasePath);
+            for (const testCaseName of testCases) {
+                const inputTSX = fs.readFileSync(`${testCasesBasePath}/${testCaseName}/input.tsx`);
+                const outputTSX = fs.readFileSync(`${testCasesBasePath}/${testCaseName}/output.tsx`);
+                it(testCaseName, () => {
+                    const expectedOutputTSX = transformAndPrettyPrint(outputTSX.toString(), []);
+                    const actualOutputTSX = transformAndPrettyPrint(inputTSX.toString(), [useCallbackTranformer]);
+                    expect(actualOutputTSX).equal(expectedOutputTSX);
+                });
+            }
         });
     }
 });
