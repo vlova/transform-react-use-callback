9e9429a5c7dc6ec6a6196edc67034ed9bb1bd622
vlova <nsinreal+vlova@gmail.com>
2020-05-11 02:52:45 +0300

    Added checking if we are in context of React.Component class

--- src/useCallbackTransformer/findAndUpdateReactSFCComponents.tsx
+++ src/useCallbackTransformer/findAndUpdateReactSFCComponents.tsx
@@ -2,6 +2,7 @@ import * as ts from 'typescript';
 import { assert } from 'ts-essentials';
 import { isReactSFCComponent } from '../common';
 import { visitReactSFCComponent } from './visitReactSFCComponent';
+import { getFullyQualifiedName } from '../common/getFullyQualifiedName';
 
 // TODO: support normal functions (not arrow only)
 // TODO: We should also reject cases like `class MyComponent { render = () => <></> }` - i.e. when function is located inside of class
@@ -9,6 +10,18 @@ import { visitReactSFCComponent } from './visitReactSFCComponent';
 export function findAndUpdateReactSFCComponents(file: ts.SourceFile, ctx: ts.TransformationContext, program: ts.Program) {
     const visitor: ts.Visitor = node => {
         switch (node.kind) {
+            case ts.SyntaxKind.ClassDeclaration: {
+                assert(ts.isClassDeclaration(node));
+                const typeChecker = program.getTypeChecker();
+                const type = typeChecker.getTypeAtLocation(node);
+                const isReactComponent = checkIfTypeExtends(type, "React.Component");
+                if (isReactComponent) {
+                    return node;
+                } else {
+                    return ts.visitEachChild(node, visitor, ctx);
+                }
+            }
+
             case ts.SyntaxKind.FunctionDeclaration:
             case ts.SyntaxKind.FunctionExpression:
             case ts.SyntaxKind.ArrowFunction: {
@@ -31,4 +44,11 @@ export function findAndUpdateReactSFCComponents(file: ts.SourceFile, ctx: ts.Tra
     }
 
     return ts.visitNode(file, visitor);
+}
+
+function checkIfTypeExtends(type: ts.Type, baseClassName: string) {
+    const baseTypes = type.getBaseTypes() || [];
+    const qualifiedNames = baseTypes.map(type => getFullyQualifiedName(type.symbol));
+    const isReactComponent = qualifiedNames.find(name => name === baseClassName);
+    return isReactComponent;
 }
--- tests/useCallbackTestCases/whenToUse/shouldNotUseCallbackInClassPureComponent/output.tsx
+++ tests/useCallbackTestCases/whenToUse/shouldNotUseCallbackInClassPureComponent/output.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-class SomeComponent extends React.Component<{ do: () => void }> {
+class SomeComponent extends React.PureComponent<{ do: () => void }> {
     render() {
         const doSomething = this.props.do;
         return (
